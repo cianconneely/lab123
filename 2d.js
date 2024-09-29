149293b5fe612f41cf1fe85d33e611741a0a37fa
cianconneely <G00390376@atu.ie>
2024-09-29 18:43:59 +0000

    2d complete

--- 2d.js
+++ 2d.js
@@ -1,9 +1,20 @@
+//declare Variables
+
 const ages = [25, 31, 42, 77];
 
+//Use a Map Function
 let d = ages.map((item)=>{
 
-    //multiplys by 2
-return item * 2;
+    //multiplys by 2 if item less than 70
+    if(item < 70){
+return item * 2;}
+
+//returns if over 70
+else {
+return item;
+}
+
 })
 
+//output 
 console.log(d)
