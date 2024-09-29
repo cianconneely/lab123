a1205ba113fc6b60feebef2e2bde2b37033498e4
cianconneely <G00390376@atu.ie>
2024-09-29 19:21:09 +0000

    3b and 3c complete

---
+++ 3b.js
@@ -0,0 +1,51 @@
+//creating an array of strings
+
+let Tasks = [];
+
+// creating addTask Function
+let addTask = (task)=>{
+
+    Tasks.push(task);
+    console.log(task + "has been added to my Tasks");
+    return Tasks.length;
+}
+
+//adding tasks to array 
+addTask("Football");
+addTask("Hurling");
+addTask("Soccer");
+addTask("Hockey");
+addTask("Baseball");
+addTask("Cricket");
+addTask("Fuseball");
+addTask("Frisbee");
+
+console.log("\n");
+
+//List all tasks function
+let listAllTasks = (task)=>{
+
+    Tasks.forEach((element)=>{
+        console.log(element);
+    })
+}
+
+//creating a delete task function 
+let deleteTask = (task)=>{
+
+    let index = Tasks.indexOf(task);
+    if(index > -1){
+
+        Tasks.splice(index,1);
+        console.log(task+ " has been deleted from my tasks");
+        }
+
+        else {
+
+            console.log(task+ " not found in array");
+
+        }
+
+        return Tasks.length;
+
+}
